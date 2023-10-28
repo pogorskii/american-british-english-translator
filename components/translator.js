98e73c1aff9c9aea2a93f722e6725154d5fc83da
Stanislav <stanislav.pogorskii@gmail.com>
2023-10-29 00:27:28 +0400

    feature complete

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,134 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const britishOnly = require("./british-only.js");
+
+const terms = [];
+Object.keys(americanOnly).forEach((key) => {
+  terms.push([key, americanOnly[key]]);
+});
+Object.keys(americanToBritishSpelling).forEach((key) => {
+  terms.push([key, americanToBritishSpelling[key]]);
+});
+Object.keys(britishOnly).forEach((key) => {
+  terms.push([britishOnly[key], key]);
+});
 
 class Translator {
+  americanToBritish(text) {
+    let translation = text;
 
+    terms.forEach((term) => {
+      const regexAmerican = new RegExp(`\\b${term[0]}\\b`, "g");
+      const regexAmericanCap = new RegExp(
+        `\\b${term[0].charAt(0).toUpperCase() + term[0].slice(1)}\\b`,
+        "g"
+      );
+
+      translation = translation.replace(
+        regexAmerican,
+        `<span class="highlight">${term[1]}</span>`
+      );
+      translation = translation.replace(
+        regexAmericanCap,
+        `<span class="highlight">${
+          term[1].charAt(0).toUpperCase() + term[1].slice(1)
+        }</span>`
+      );
+    });
+
+    const timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]))/g;
+    const times = translation.match(timeRegex);
+    if (times) {
+      times.forEach((time) => {
+        translation = translation.replace(
+          time,
+          `<span class="highlight">${time.replace(":", ".")}</span>`
+        );
+      });
+    }
+
+    const titleRegex = /(mr|mrs|ms|mx|dr|prof)\./gi;
+    const titles = translation.match(titleRegex);
+    if (titles) {
+      titles.forEach((title) => {
+        translation = translation.replace(
+          title,
+          `<span class="highlight">${(
+            title.charAt(0).toUpperCase() + title.slice(1)
+          ).replace(".", "")}</span>`
+        );
+      });
+    }
+
+    return translation;
+  }
+
+  britishToAmerican(text) {
+    const translation = text;
+
+    terms.forEach((term) => {
+      const regexBritish = new RegExp(`\\b${term[1]}\\b`, "g");
+      const regexBritishCap = new RegExp(
+        `\\b${term[1].charAt(0).toUpperCase() + term[1].slice(1)}\\b`,
+        "g"
+      );
+
+      translation = translation.replace(
+        regexBritish,
+        `<span class="highlight">${term[0]}</span>`
+      );
+      translation = translation.replace(
+        regexBritishCap,
+        `<span class="highlight">${
+          term[0].charAt(0).toUpperCase() + term[0].slice(1)
+        }</span>`
+      );
+    });
+
+    const timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(\.)([0-5][0-9]))/g;
+    const times = translation.match(timeRegex);
+    if (times) {
+      times.forEach((time) => {
+        translation = translation.replace(
+          time,
+          `<span class="highlight">${time.replace(".", ":")}</span>`
+        );
+      });
+    }
+
+    const titleRegex = /(mr|mrs|ms|mx|dr|prof)\s/gi;
+    const titles = translation.match(titleRegex);
+    if (titles) {
+      titles.forEach((title) => {
+        translation = translation.replace(
+          title,
+          `<span class="highlight">${(
+            title.charAt(0).toUpperCase() + title.slice(1)
+          ).replace(" ", ".")}</span> `
+        );
+      });
+    }
+
+    return translation;
+  }
+
+  translate(text, locale) {
+    let translation;
+
+    if (locale === "american-to-british") {
+      translation = this.americanToBritish(text);
+    }
+
+    if (locale === "british-to-american") {
+      translation = this.britishToAmerican(text);
+    }
+
+    if (translation === text) {
+      translation = "Everything looks good to me!";
+    }
+
+    return translation;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,20 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (!text) return res.json({ error: "No text to translate" });
+    if (!text || !locale)
+      return res.json({ error: "Required field(s) missing" });
+    if (locale != "american-to-british" && locale != "british-to-american")
+      return res.json({ error: "Invalid value for locale field" });
+
+    const translation = translator.translate(text, locale);
+    return res.json({ text, translation });
+  });
 };
